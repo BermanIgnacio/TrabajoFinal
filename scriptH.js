e55416e0386ca5add1fd1b60172e967e4700cd04
BermanIgnacio <bermanignacio94@hotmail.com>
2022-07-16 17:53:19 -0300

    Avances

---
+++ scriptH.js
@@ -0,0 +1,17 @@
+let video = document.querySelector("video");
+
+video.onloadedmetadata = function() {
+    let time = document.getElementById("tiempo");
+    time.textContent=" Duracion video : "+this.duration.toFixed(2);
+};
+
+let botones = document.querySelectorAll("button");
+let srcs = document.querySelector("source");
+
+botones[0].addEventListener("click",()=>{
+    video.play();
+});
+
+botones[1].addEventListener("click",()=>{
+    video.pause();
+})
